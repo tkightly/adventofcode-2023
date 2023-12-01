82983b0f3f69f50b54d036c1cccd5d7557d444e1
Tim Kightly <tim@tkightly.uk>
2023-12-01 13:21:16 +0000

    lint via black

--- 1/day_1_part_2.py
+++ 1/day_1_part_2.py
@@ -1,5 +1,5 @@
 """https://adventofcode.com/2023/day/1"""
-import re # regex module
+import re  # regex module
 
 WORDTODIGIT = {
     "one": 1,
@@ -10,26 +10,28 @@ WORDTODIGIT = {
     "six": 6,
     "seven": 7,
     "eight": 8,
-    "nine": 9
+    "nine": 9,
 }
 
-with open('1/input.txt', 'r', encoding="utf-8") as input_file:
+with open("1/input.txt", "r", encoding="utf-8") as input_file:
     total = 0
 
     for row in input_file:
-
         row_int_array = []
 
         # match each digit and word within current row
-        for match_group in re.finditer('(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))',\
-row):
+        for match_group in re.finditer(
+            "(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))", row
+        ):
             digit = match_group.group(1)
 
             # if the current match is a word, replace it with a number
             # we are casting the ints as str, because we want += to concatenate them not add them
             # together
             for word_to_replace in WORDTODIGIT:
-                digit = digit.replace(word_to_replace, str(WORDTODIGIT[word_to_replace]))
+                digit = digit.replace(
+                    word_to_replace, str(WORDTODIGIT[word_to_replace])
+                )
 
             row_int_array.append(digit)
 
