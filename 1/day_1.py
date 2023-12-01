79551ee25986271e6c74d2ebaf9b4c097cd1c94f
Tim Kightly <40869890+tkightly@users.noreply.github.com>
2023-12-01 11:16:32 +0000

    solve 1 (#1)
    * solve 1
    
    * solve
    
    * lint
    
    * disable some annoying lint
    
    * move pylintrc

---
+++ 1/day_1.py
@@ -0,0 +1,24 @@
+"""https://adventofcode.com/2023/day/1"""
+import re
+with open('1/input.txt', 'r', encoding="utf-8") as input_file:
+    total = 0
+
+    for row in input_file:
+
+        row_int_array = []
+        row_calibration_value = ""
+
+        for character in row:
+            character = re.search('[0-9]', character)
+            if character:
+                row_int_array.append(int(character.group(0)))
+
+        row_calibration_value += str(row_int_array[0])
+        row_calibration_value += str(row_int_array[-1])
+
+        total += int(row_calibration_value)
+        print(f"ints are {row_int_array}, {row_int_array[0]} and {row_int_array[-1]} \
+    equals {row_calibration_value}")
+
+    print(f"total is {total}")
+input_file.close()
