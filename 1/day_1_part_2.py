0cac799c65039f2737a63d8252924603e6f72ae5
Tim Kightly <tim@tkightly.uk>
2023-12-01 13:34:34 +0000

    lint

--- 1/day_1_part_2.py
+++ 1/day_1_part_2.py
@@ -21,8 +21,7 @@ with open("1/input.txt", "r", encoding="utf-8") as input_file:
 
         # match each digit and word within current row
         for match_group in re.finditer(
-            "(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))", row
-        ):
+            "(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))", row):
             digit = match_group.group(1)
 
             # if the current match is a word, replace it with a number
